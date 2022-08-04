29562a3cc6a8c37372f69719badd692cdbf4b29e
kimdaeki <kimdaeki@gmail.com>
2022-08-04 09:02:14 +0900

    1

--- src/main/java/com/iu/start/test/BankBookTestMain.java
+++ src/main/java/com/iu/start/test/BankBookTestMain.java
@@ -1,9 +1,22 @@
 package com.iu.start.test;
 
+import com.iu.start.bankBook.BankBookDAO;
+import com.iu.start.bankBook.BankBookDTO;
+
 public class BankBookTestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BankBookDAO bankBookDAO = new BankBookDAO();
+		BankBookDTO bankBookDTO = new BankBookDTO();
+		bankBookDTO.setBookNum(111L);
+		try {
+			bankBookDTO	= bankBookDAO.getDetail(bankBookDTO);
+			
+			System.out.println(bankBookDTO != null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
